01a9bf727f688d214365a4bb0ae4348af27bbd99
prostiak2019 <yarovenkodmv@gmail.com>
2019-10-15 18:40:05 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      let timeStuding=0;
+      let timeStuding = 0;
 
+      // check the total time depending on the programming knowledge 'knowsProgramming'
       if (knowsProgramming) {
-       timeStuding = 800;
-      } else { timeStuding = 800 + 500;}
+        timeStuding = 800;
+        } else { timeStuding = 800 + 500;
+      };
 
+      // count the number of weeks 
       let weeksStuding = timeStuding/config[focus];
+
+      // round and return value
       return Math.ceil(weeksStuding);
   };
   
